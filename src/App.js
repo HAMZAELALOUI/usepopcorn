90dd78b3b73584d7d8828bb02249c61c257880f1
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-07-09 14:42:41 +0100

    Cleaning Up Data Fetching

--- src/App.js
+++ src/App.js
@@ -79,20 +79,25 @@ export default function App() {
 
   useEffect(
     function(){
+      const controller= new AbortController()
       async function fetchMovies(){
         try{
         setIsLoading(true);
         setError('')
 
-      const res=await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`) ;
+      const res=await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,{signal:controller.signal}) ;
       if(!res.ok) throw new Error ("Please check your network ...");
 
         const data =await res.json();
         if (data.Response === "False") throw new Error("Movie not found");
         setMovies(data.Search)
+        setError("")
       } catch (err){
         console.error(err.message)
-        setError(err.message)
+        if(err.name!== 'AbortError'){
+          setError(err.message)
+
+        }
 
       }finally{
         setIsLoading(false)
@@ -102,7 +107,10 @@ export default function App() {
         setError("");
         return;
       }
-    fetchMovies() },[query])
+    fetchMovies()
+  return function(){
+    controller.abort()
+  } },[query])
 
 
         
